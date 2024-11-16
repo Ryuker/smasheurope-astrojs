60389d803824f040879b32220c79837bd099e65b
ryuker <lifeisagamenotaproduct@gmail.com>
2024-11-16 15:38:53 +0100

    added m size to fontSize in tailwind config, tweaked font sizes

--- new-version/smasheurope.com/tailwind.config.mjs
+++ new-version/smasheurope.com/tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontSize: {
+				m: '.9375rem'
+			},
 			colors: {
 				'se-link':  {
 					200: '#2c7be5',
