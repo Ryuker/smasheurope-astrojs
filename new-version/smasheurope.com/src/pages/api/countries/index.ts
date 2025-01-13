742fcff7f5ac22deb62c971b8f1b49755a2a91e0
ryuker <lifeisagamenotaproduct@gmail.com>
2025-01-13 14:09:55 +0100

    retrieved countries from api endpoint, passed down countries as prop

--- new-version/smasheurope.com/src/pages/api/countries/index.ts
+++ new-version/smasheurope.com/src/pages/api/countries/index.ts
@@ -3,13 +3,14 @@ import type { APIRoute } from "astro"
 
 // Get all events handler, returns all events from the api_endpoint
 export const GET: APIRoute = async ({ params, request }) => {
-  console.log('Get request received');
+  console.log('Country Get request received');
 
   // Fetch the data from the external API
   const api_endpoint = 'http://localhost:5000/api/countries';
   const uri = `${api_endpoint}`;
   const response = await fetch(uri);
   const countriesData = await response.json();
+  console.log(countriesData);
 
   const data = countriesData;
 
@@ -52,25 +53,25 @@ export const POST: APIRoute = async ({ params, request }) => {
   });
 }
 
-// for static version only
-export async function getStaticPaths() {
-  const inStaticMode = await import.meta.env.STATIC_MODE;
-  if (inStaticMode === "false"){
-    return [];
-  }
+// // for static version only
+// export async function getStaticPaths() {
+//   const inStaticMode = await import.meta.env.STATIC_MODE;
+//   if (inStaticMode === "false"){
+//     return [];
+//   }
   
-  // Fetch the data from the external API
-  const api_endpoint = 'http://localhost:5000/countries';
-  const uri = `${api_endpoint}`;
-  const response = await fetch(uri);
-  const countriesData = await response.json();
+//   // Fetch the data from the external API
+//   const api_endpoint = 'http://localhost:5000/countries';
+//   const uri = `${api_endpoint}`;
+//   const response = await fetch(uri);
+//   const countriesData = await response.json();
 
-  const data = countriesData;
+//   const data = countriesData;
 
-  return data.flatMap(country => {
-    return [
-      { params: { countryId: country.id } },
-    ]
-  });
-}
+//   return data.flatMap(country => {
+//     return [
+//       { params: { countryId: country.id } },
+//     ]
+//   });
+// }
 
